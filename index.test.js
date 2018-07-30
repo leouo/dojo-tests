bc802819f6bf5e71a3fb6f512962a56ca02c5076
Leonardo Cesar Morais <leonardoccpmorais@gmail.com>
2018-07-30 19:07:14 -0300

    add helper function to set mocks

--- index.test.js
+++ index.test.js
@@ -3,37 +3,35 @@ require('jasmine');
 let imc = require('./imc');
 let nock = require('nock')
 
-let IMCMock = {};
+const setAPIMock = mockPayload =>
+  nock('http://localhost:8888/')
+    .get('/imc')
+    .query(true)
+    .reply(200, mockPayload);
+
 
 describe('O módulo de IMC:', () => {
-    beforeEach(() => {
-        nock('http://localhost:8888/')
-            .get('/imc')
-            .query(true)
-            .reply(200, IMCMock);
-    });
+  afterAll(() => {
+    nock.cleanAll();
+  });
 
-    afterEach(() => {
-        nock.cleanAll();
-    });
+  it('Deve calcular o IMC com base no peso e altura', async () => {
+    setAPIMock({ imc: 23.89 });
+    expect(await imc.getIMC(80, 1.83)).toEqual({ imc: 23.89});
+  });
 
-    IMCMock = { imc: 23.89 };
-    it('Deve calcular o IMC com base no peso e altura', async () => {
-        expect(await imc.getIMC(80, 1.83)).toEqual({ imc: 23.89});
-    });
+  it('Deve retornar a classificação "Magro"', async () => {
+    setAPIMock({ imc: 15 });
+    expect(await imc.getClassification(50, 1.83)).toBe('Magro');
+  });
 
-    IMCMock = { imc: 15 };
-    it('Deve retornar a classificação "Magro"', async () => {
-        expect(await imc.getClassification(50, 1.83)).toBe('Magro');
-    });
+  it('Deve retornar a classificação "Normal"', async () => {
+    setAPIMock({ imc: 23 });
+    expect(await imc.getClassification(80, 1.83)).toBe('Normal');
+  });
 
-    IMCMock = { imc: 23 };
-    it('Deve retornar a classificação "Normal"', async () => {
-        expect(await imc.getClassification(80, 1.83)).toBe('Normal');
-    });
-
-    IMCMock = { imc: 50 };
-    it('Deve retornar a classificação "Sobrepeso"', async () => {
-        expect(await imc.getClassification(90, 1.83)).toBe('Sobrepeso');
-    });
+  it('Deve retornar a classificação "Sobrepeso"', async () => {
+    setAPIMock({ imc: 50 });
+    expect(await imc.getClassification(90, 1.83)).toBe('Sobrepeso');
+  });
 });
